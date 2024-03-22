58f7f9f500cd999da81ef068f6f3abf81e703817
Solo Super <11663739+solosuper@users.noreply.github.com>
2024-03-22 07:17:42 +0100

    Realsense virker

--- visualisation.py
+++ visualisation.py
@@ -100,7 +100,7 @@ def add_frustum(plotter, camera_model, image_size, scale=0.1):
     pyramid = pv.Pyramid([point_bottom_left, point_bottom_right, point_top_left, point_top_right, point_focal])
     pyramid.transform(S)
 
-    rectangle = pv.Rectangle([point_bottom_left, point_bottom_right, point_top_left, point_top_right])
+    rectangle = pv.Rectangle([point_bottom_left, point_bottom_right, point_top_left])
     rectangle.texture_map_to_plane(inplace=True)
     rectangle.transform(S)
 
